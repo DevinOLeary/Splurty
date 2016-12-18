88ced37ad1dd201ee1aae5e7a99ae2fe782597fd
Devin O'Leary <djoleary07@gmail.com>
2016-12-18 01:19:17 +0000

    about page

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -11,8 +11,12 @@ class QuotesController < ApplicationController
     @quote = Quote.create(quote_params)
     if @quote.invalid?
       flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
-    end 
+    end
     redirect_to root_path
+  end
+
+  def about
+
   end
 
   private
@@ -20,6 +24,8 @@ class QuotesController < ApplicationController
   def quote_params
     params.require(:quote).permit(:saying, :author)
   end
+
+
 
 
 end
